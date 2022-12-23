e1c6e317f2a4decaa8b3efdc1a5db56ec9193cde
Eric <sanceric92@gmail.com>
2022-12-23 16:35:46 +0100

    Add MaterialCheckbox.js

---
+++ src/components/custom-components/material-checkbox/MaterialCheckbox.js
@@ -0,0 +1,14 @@
+import "./MaterialCheckbox.css"
+
+const MaterialCheckbox = () => {
+    return (
+        <label>
+            <input id={"chch"} type={"checkbox"}/>
+            <span className={"checkbox"}>
+            </span>
+        </label>
+    )
+
+}
+
+export default MaterialCheckbox

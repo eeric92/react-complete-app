69e53e2a7fb05d6322a48c7342587e4f02846541
Eric <sanceric92@gmail.com>
2022-12-23 11:51:37 +0100

    Changed ExpensesList.js - Added h3

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -2,22 +2,28 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
-  if (props.items.length === 0) {
-    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
-  }
 
-  return (
-    <ul className="expenses-list">
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </ul>
-  );
+    const noExpenses = <div>
+        <h1 className="expenses-list__fallback">Found no expenses</h1>
+        <h3 className={"expenses-list__fallback"}>You can add a new expense by clicking on Add New Expense button</h3>
+    </div>
+
+    if (props.items.length === 0) {
+        return noExpenses;
+    }
+
+    return (
+        <ul className="expenses-list">
+            {props.items.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))}
+        </ul>
+    );
 };
 
 export default ExpensesList;
